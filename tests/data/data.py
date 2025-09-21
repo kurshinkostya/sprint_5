b49c0436468b9b06be0544fa307a8cab4e06d8df
Konstantin Kurshin <ekurshin@gmail.com>
2025-09-21 20:06:22 +0300

    add test and data

---
+++ tests/data/data.py
@@ -0,0 +1,13 @@
+import random
+
+
+class PersonData:
+    user_name = 'kostya'
+    login = 'kostya_k_30_666@ya.ru'
+    password = '315920'
+
+# функция генератора логина
+class ValidData:
+    user_name = 'Test test'
+    login = f"Test_test{random.randint(10, 999)}@ya.ru"
+    password = f"{random.randint(100, 999)}{random.randint(100, 999)}"
---
+++ tests/data/urls.py
@@ -0,0 +1,15 @@
+class Urls:
+    # основная страница
+    url_main_paige = "https://stellarburgers.nomoreparties.site/"
+
+    # страница входа
+    url_login = "https://stellarburgers.nomoreparties.site/login"
+
+    # форма Личный кабинет -> Профиль
+    url_profile = "https://stellarburgers.nomoreparties.site/account/profile"
+
+    # страница регистрации
+    url_register = "https://stellarburgers.nomoreparties.site/register"
+
+    # страница забыл пароль
+    url_forgot_password = "https://stellarburgers.nomoreparties.site/forgot-password"
